0fc430c4f5212c31d399dd3f66b1a3ad822f9286
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-09-09 21:00:18 +1200

    test segmented source

---
+++ parablevr.game.api/users/DoRegisterUser.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
+namespace parablevr.game.api.users
+{
+  public static class DoRegisterUser
+  {
+    [FunctionName("DoRegisterUser")]
+    public static IActionResult DoRegisterUserAsync(
+      [HttpTrigger(AuthorizationLevel.Function, "post", Route = "users/do/register")]HttpRequest req,
+      ILogger log)
+    {
+      log.LogInformation("C# HTTP trigger function processed a request.");
+
+      string name = req.Query["name"];
+
+      string requestBody = new StreamReader(req.Body).ReadToEnd();
+      dynamic data = JsonConvert.DeserializeObject(requestBody);
+      name = name ?? data?.name;
+
+      return name != null
+          ? (ActionResult)new OkObjectResult($"Hello, {name}")
+          : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+    }
+  }
+}
