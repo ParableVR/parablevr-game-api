3ad6a24a3d5108ca44288aa05d5d097d207701f7
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-09-14 14:39:48 +1200

    get a single location

---
+++ parablevr.game.api/locations/GetLocation.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using parablevr.game.api.objects;
+using parablevr.game.api.utilities;
+
+namespace parablevr.game.api.users
+{
+  public static class GetLocation
+  {
+    [FunctionName("getLocationByID")]
+    public static async Task<IActionResult> GetUserByIDAsync(
+      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "locations/get/location/id/{lid}")]HttpRequest req,
+      string lid,
+      ILogger log)
+    {
+      // set up connection
+      DataClient client = new DataClient();
+      IMongoCollection<Location> locations = client.GetCollection<Location>("locations");
+
+      List<Location> locResults = (await (await locations.FindAsync(
+        x => x.id == lid && x.when_deleted == null)).ToListAsync());
+
+      if (locResults.Count == 0)
+      {
+        return new NotFoundObjectResult(new
+        {
+          message = "Location not found"
+        });
+      }
+
+      return new OkObjectResult(new
+      {
+        message = "Found location",
+        location = locResults.First()
+      });
+    }
+  }
+}
