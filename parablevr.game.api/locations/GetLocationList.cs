0761788894fa76d7e1f5650f0aa85eb1caca1ffb
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-09-14 15:02:54 +1200

    list locations

--- parablevr.game.api/locations/GetLocationList.cs
+++ parablevr.game.api/locations/GetLocationList.cs
@@ -17,33 +17,38 @@ using parablevr.game.api.utilities;
 
 namespace parablevr.game.api.locations
 {
-  public static class GetLocation
+  public static class GetLocationList
   {
-    [FunctionName("getLocationByID")]
-    public static async Task<IActionResult> GetLocationByIDAsync(
-      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "locations/get/location/id/{lid}")]HttpRequest req,
-      string lid,
+    [FunctionName("getLocationList")]
+    public static async Task<IActionResult> GetLocationListAsync(
+      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "locations/get/location/list")]HttpRequest req,
       ILogger log)
     {
       // set up connection
       DataClient client = new DataClient();
       IMongoCollection<Location> locations = client.GetCollection<Location>("locations");
 
-      List<Location> locResults = (await (await locations.FindAsync(
-        x => x.id == lid && x.when_deleted == null)).ToListAsync());
+      List<Location> locResults = await locations.Find(x => x.when_deleted == null)
+        .Project<Location>(
+          Builders<Location>.Projection
+            .Include(x => x.id)
+            .Include(x => x.name)
+            .Include(x => x.max_players)
+            .Include(x => x.when_created))
+        .ToListAsync();
 
       if (locResults.Count == 0)
       {
         return new NotFoundObjectResult(new
         {
-          message = "Location not found"
+          message = "No locations found"
         });
       }
 
       return new OkObjectResult(new
       {
-        message = "Found location",
-        location = locResults.First()
+        message = "Found locations",
+        locations = locResults
       });
     }
   }
