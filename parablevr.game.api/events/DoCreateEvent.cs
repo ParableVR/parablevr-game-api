3890d903f9fd917e4ffa3a15f9c15975ce89462d
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-10-04 11:24:46 +1300

    fix create event

--- parablevr.game.api/events/DoCreateEvent.cs
+++ parablevr.game.api/events/DoCreateEvent.cs
@@ -18,86 +18,88 @@ using parablevr.game.api.utilities;
 
 namespace parablevr.game.api.sessions
 {
-    public static class DoCreateEvent
+  public static class DoCreateEvent
+  {
+    [FunctionName("doCreateEvent")]
+    public static async Task<IActionResult> DoCreateEventAsync(
+      [HttpTrigger(AuthorizationLevel.Function, "post", Route = "events/do/create/{eventSession}")]HttpRequest req,
+      string eventSession,
+      ILogger log)
     {
-        [FunctionName("doCreateEvent")]
-        public static async Task<IActionResult> DoCreateEventAsync(
-          [HttpTrigger(AuthorizationLevel.Function, "post", Route = "events/do/create/{eventSession}")]HttpRequest req,
-          string eventSession,
-          ILogger log)
+      // set up connection
+      DataClient client = new DataClient();
+      IMongoCollection<Event> events = client.GetCollection<Event>("events");
+
+      // deserialise body input into class
+      string reqBody;
+      EventObject @event;
+      try
+      {
+        reqBody = await new StreamReader(req.Body).ReadToEndAsync();
+        if (string.IsNullOrEmpty(reqBody))
         {
-            // set up connection
-            DataClient client = new DataClient();
-            IMongoCollection<Event> events = client.GetCollection<Event>("events");
-
-            // deserialise body input into class
-            string reqBody;
-            EventObject @event;
-            try
-            {
-                reqBody = await new StreamReader(req.Body).ReadToEndAsync();
-                if (string.IsNullOrEmpty(reqBody))
-                {
-                    return new BadRequestObjectResult(new
-                    {
-                        message = "No request body supplied"
-                    });
-                }
-
-                @event = JsonConvert.DeserializeObject<EventObject>(reqBody);
-            }
-            catch (Exception e)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    message = e.Message
-                });
-            }
-
-            // valid input
-            bool input_valid = true;
-            if (string.IsNullOrEmpty(eventSession))
-            {
-                input_valid = false;
-            }
-
-            if (@event.type != "interaction" || @event.type != "perception" || @event.type != "reaction")
-            {
-                input_valid = false;
-            }
-
-            // put the pins in if invalid input
-            if (!input_valid)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    message = "Invalid input"
-                });
-            }
-
-            @event.when_occured = DateTime.UtcNow;
-
-            FilterDefinition<Event> filter = Builders<Event>
-              .Filter.Eq(x => x.session, eventSession);
-            UpdateDefinition<Event> update = Builders<Event>.Update
-              .Push<EventObject>(x => x.events, new EventObject()
-              {
-                  type = @event.type.ToLower(),
-                  user = null,
-                  when_occured = DateTime.UtcNow,
-                  contact_duration = @event.contact_duration,
-                  result = @event.result,
-                  object_coordinator = @event.object_coordinator,
-                  objects_involved = @event.objects_involved
-              });
-
-            await events.FindOneAndUpdateAsync(filter, update);
-
-            return new OkObjectResult(new
-            {
-                message = "Successfully created an event",
-                @event
-            });
+          return new BadRequestObjectResult(new
+          {
+            message = "No request body supplied"
+          });
         }
+
+        @event = JsonConvert.DeserializeObject<EventObject>(reqBody);
+      }
+      catch (Exception e)
+      {
+        return new BadRequestObjectResult(new
+        {
+          message = e.Message
+        });
+      }
+
+      // valid input
+      bool input_valid = true;
+      if (string.IsNullOrEmpty(eventSession))
+      {
+        input_valid = false;
+      }
+
+      if (!(@event.type == "interaction" || @event.type == "perception" || @event.type == "reaction"))
+      {
+        input_valid = false;
+      }
+
+      // put the pins in if invalid input
+      if (!input_valid)
+      {
+        return new BadRequestObjectResult(new
+        {
+          message = "Invalid input"
+        });
+      }
+
+      @event.when_occured = DateTime.UtcNow;
+      EventObject eventBuilt = new EventObject()
+      {
+        id = ObjectId.GenerateNewId().ToString(),
+        type = @event.type.ToLower(),
+        user = null,
+        when_occured = DateTime.UtcNow,
+        contact_duration = @event.contact_duration,
+        result = @event.result,
+        object_coordinator = @event.object_coordinator,
+        objects_involved = @event.objects_involved
+      };
+
+      FilterDefinition<Event> filter = Builders<Event>
+        .Filter.Eq(x => x.id, eventSession);
+      UpdateDefinition<Event> update = Builders<Event>.Update
+        .Push<EventObject>(x => x.events, eventBuilt);
+
+      await events.UpdateOneAsync(filter, update);
+
+      return new OkObjectResult(new
+      {
+        message = "Successfully created an event",
+        @event = eventBuilt
+      });
     }
+  }
 }
--- parablevr.game.api/events/DoCreateEventSession.cs
+++ parablevr.game.api/events/DoCreateEventSession.cs
@@ -18,65 +18,65 @@ using parablevr.game.api.utilities;
 
 namespace parablevr.game.api.sessions
 {
-    public static class DoCreateEventSession
+  public static class DoCreateEventSession
+  {
+    [FunctionName("doCreateEventSession")]
+    public static async Task<IActionResult> DoCreateEventSessionAsync(
+      [HttpTrigger(AuthorizationLevel.Function, "post", Route = "events/do/create/session")]HttpRequest req,
+      ILogger log)
     {
-        [FunctionName("doCreateEventSession")]
-        public static async Task<IActionResult> DoCreateEventSessionAsync(
-          [HttpTrigger(AuthorizationLevel.Function, "post", Route = "events/do/create/session")]HttpRequest req,
-          ILogger log)
+      // set up connection
+      DataClient client = new DataClient();
+      IMongoCollection<Event> events = client.GetCollection<Event>("events");
+
+      // deserialise body input into class
+      string reqBody;
+      Event eventSession;
+      try
+      {
+        reqBody = await new StreamReader(req.Body).ReadToEndAsync();
+        if (string.IsNullOrEmpty(reqBody))
         {
-            // set up connection
-            DataClient client = new DataClient();
-            IMongoCollection<Event> events = client.GetCollection<Event>("events");
-
-            // deserialise body input into class
-            string reqBody;
-            Event eventSession;
-            try
-            {
-                reqBody = await new StreamReader(req.Body).ReadToEndAsync();
-                if (string.IsNullOrEmpty(reqBody))
-                {
-                    return new BadRequestObjectResult(new
-                    {
-                        message = "No request body supplied"
-                    });
-                }
-
-                eventSession = JsonConvert.DeserializeObject<Event>(reqBody);
-            }
-            catch (Exception e)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    message = e.Message
-                });
-            }
-
-            // valid input
-            bool input_valid = true;
-            if (string.IsNullOrEmpty(eventSession.session)) input_valid = false;
-            //if (eventSession.events.Count > 0) input_valid = false; // do not allow specifying events here
-
-            // put the pins in if invalid input
-            if (!input_valid)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    message = "Invalid input"
-                });
-            }
-
-            eventSession.when_started = DateTime.UtcNow;
-            eventSession.when_deleted = null;
-
-            await events.InsertOneAsync(eventSession);
-
-            return new OkObjectResult(new
-            {
-                message = "Successfully created an event session",
-                event_session = eventSession
-            });
+          return new BadRequestObjectResult(new
+          {
+            message = "No request body supplied"
+          });
         }
+
+        eventSession = JsonConvert.DeserializeObject<Event>(reqBody);
+      }
+      catch (Exception e)
+      {
+        return new BadRequestObjectResult(new
+        {
+          message = e.Message
+        });
+      }
+
+      // valid input
+      bool input_valid = true;
+      if (string.IsNullOrEmpty(eventSession.session)) input_valid = false;
+      //if (eventSession.events.Count > 0) input_valid = false; // do not allow specifying events here
+
+      // put the pins in if invalid input
+      if (!input_valid)
+      {
+        return new BadRequestObjectResult(new
+        {
+          message = "Invalid input"
+        });
+      }
+
+      eventSession.when_started = DateTime.UtcNow;
+      eventSession.when_deleted = null;
+
+      await events.InsertOneAsync(eventSession);
+
+      return new OkObjectResult(new
+      {
+        message = "Successfully created an event session",
+        event_session = eventSession
+      });
     }
+  }
 }
